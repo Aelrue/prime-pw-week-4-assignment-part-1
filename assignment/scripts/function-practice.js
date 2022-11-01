177b5ab3d096ab226ed6af3b8915fb232036fd07
Alexis Mike <littlehoatzin@gmail.com>
2022-11-01 18:26:08 -0500

    revisited #5 and #6

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -37,12 +37,13 @@ console.log(multiplyThree(6, 10, 3));
 //    or greater than zero, and false otherwise
 function isPositive(number) {
   if (number > 0) {
-    return "True";
+    return true;
   } else {
-    return "False";
+    return false;
   }
 }
 console.log(isPositive(10));
+console.log(isPositive(-2));
 // Call the function to test each outcome (true & false)
 // Write a separate console.log statement for each outcome
 console.log("isPositive - should say true", isPositive(3));
@@ -53,7 +54,7 @@ console.log("isPositive - should say false", isPositive(-3));
 //    array is empty, return `undefined`.
 let list = ["ball", "kite", "book", "bucket"];
 function getLast(array) {
-  return array.at(-1);
+  return array[array.length - 1];
 }
 let result = getLast(list);
 console.log(result);
@@ -78,16 +79,16 @@ console.log(find(7, numbers));
 // ----------------------
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
-function isFirstLetter(letter, string) {}
-console.log("isFirstLetter - should say true", isFirstLetter("a", "apple"));
-console.log("isFirstLetter - should say false", isFirstLetter("z", "apple"));
+// function isFirstLetter(letter, string) {}
+// console.log("isFirstLetter - should say true", isFirstLetter("a", "apple"));
+// console.log("isFirstLetter - should say false", isFirstLetter("z", "apple"));
 
-// 9. Function to return the sum of all numbers in an array
-function sumAll() {
-  let sum = 0;
-  // TODO: loop to add items
-  return sum;
-}
+// // 9. Function to return the sum of all numbers in an array
+// function sumAll() {
+//   let sum = 0;
+//   // TODO: loop to add items
+//   return sum;
+// }
 
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
